21b2c13989aad3bd335f7b80ba998c75b531e281
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-02 21:54:39 -0300

    first commit

---
+++ src/main/java/com/github/pedroluiznogueira/springdockerpipeline/SpringDockerPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.springdockerpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pedroluiznogueira/springdockerpipeline/SpringDockerPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.springdockerpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
